ad5d70503881caa7881557def2a35c1d624dc2b6
Edsamn <eduardomsribas@gmail.comm>
2023-12-21 22:33:38 -0300

    commit

--- ex3/scripts.js
+++ ex3/scripts.js
@@ -1,7 +1,20 @@
 // 3. Desenvolva um algoritmo que preenche um vetor com os 4
 // primeiros números perfeitos.
-let vetor = [];
+let numerosPerfeitos = [];
+let numero = 1;
 
-for (let i = 0; i <= 4; i++) {
-    
+
+
+while (numerosPerfeitos.length < 4) {
+    let soma = 0;
+    for (let i = 1; i < numero; i++) {
+        if (numero % i === 0) {
+            soma += i;
+        }
+    }
+    if (soma === numero) {
+        numerosPerfeitos.push(numero);
+    }
+    numero++;
 }
+document.write(numerosPerfeitos +"<br>");
