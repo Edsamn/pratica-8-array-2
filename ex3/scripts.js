a073e510b470ba1d3925fc61a88ab244c2c327e5
Edsamn <eduardomsribas@gmail.comm>
2023-12-21 21:20:32 -0300

    commit

---
+++ ex3/scripts.js
@@ -0,0 +1,7 @@
+// 3. Desenvolva um algoritmo que preenche um vetor com os 4
+// primeiros números perfeitos.
+let vetor = [];
+
+for (let i = 0; i <= 4; i++) {
+    
+}
---
+++ ex4/scripts.js
@@ -0,0 +1,12 @@
+// 4. Desenvolva um algoritmo que declare uma lista de 10 números e
+// solicite um número para o usuário e no final informe se o número
+// que o usuário digitou está dentro da lista ou não.
+let vetor = [1,2,3,4,5,6,7,8,9,10]
+
+let numero = Number(prompt("digite um número"))
+
+for (let sorteado of vetor) {
+    if (sorteado == numero) {
+        console.log('Está na lista')
+    } console.log('Não está na lista')
+} 
